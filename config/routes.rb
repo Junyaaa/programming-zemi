5dbd6267612017ca7df66ac43c14a58245ad8030
“Junyaaa <bb.junjun2@gmail.com>
2022-05-09 16:39:24 +0000

    静的ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'about' => "static_pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 以下のコードを追加してみましょう
