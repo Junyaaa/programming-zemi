06abdcecff313eae11453c7f5391dbcc515b3dce
“Junyaaa” <bb.junjun2@gmail.com>
2022-05-11 12:56:36 +0000

    login and logout

--- config/routes 2.rb
+++ config/routes 2.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get  'signup' => "users#new"
   root 'static_pages#home'
   get 'home' => 'static_pages#home'
-  get 'help' =>'static_pages#help'
+  get 'heip' =>'static_pages#help'
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 以下のコードを追加してみましょう
-  get 'signup' => 'users#new'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-  resources :users
+  get 'hello-world' => "foo#hello"
+
 end
